d9eaf2fb5cf4e6f0d392a8d29cb2cddf36e184e6
Phuldev Mandal <59725190+Phuldev@users.noreply.github.com>
2024-02-03 18:09:32 +0545

    initialization:passport and types/passport-local

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,10 +13,14 @@ import { ProductModule } from './product/product.module';
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory:(configService:ConfigService)=>({uri:configService.get("MONGO_URI")}),
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
       inject: [ConfigService],
     }),
     ProductModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
